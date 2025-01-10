695ed386566412bf1a5ec8ca725f9d7de69e6a13
ManuelWestermeier <westermeier111@gmail.com>
2025-01-10 18:27:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import GitHubFS from "./gh-fs.js";
+
+// Initialize GitHubFS instance
+const githubFS = new GitHubFS({
+    authToken: process.env.GITHUB_API_TOKEN,
+    owner: "manuelwestermeier",
+    repo: "data-test-2",
+    defaultCommitter: {
+        email: "westermeier111@gmail.com",
+        name: "Manuel Westermeier",
+    },
+    encryptionKey: "my-secure-encryption-key-32!", // Use a strong, secure key
+});
+
+//exists
+await githubFS.exists("path");
+//files
+await githubFS.getFileMetadata("path");
+await githubFS.writeFile("path", "conetnt", "commitmessage/date");
+await githubFS.readFile("path");
+await githubFS.deleteFile("path", "commitmessage/date");
+await githubFS.moveFile("src-path", "dest-path", "commitmessage/date");
+await githubFS.copyFile("src-path", "dest-path", "commitmessage/date");
+//dirs
+await githubFS.createDir("dir", "commitmessage/date");
+await githubFS.readDir("dir");
+await githubFS.deleteDir("dir", "commitmessage/date");
